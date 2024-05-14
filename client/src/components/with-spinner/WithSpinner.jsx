18dd990b508964b7bb6bd1adca22febb4d5d67d2
josueace <josueacev2@gmail.com>
2024-05-14 12:51:27 -0400

    clean2

--- client/src/components/with-spinner/WithSpinner.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Spinner from './Spinner';
-
-// eslint-disable-next-line react/display-name
-const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) =>
-    isLoading ? <Spinner /> : <WrappedComponent {...otherProps} />;
-export default WithSpinner;
--- client/src/pages/register/register.styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const RegisterContainer = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-`;
